53b76df2d809aa61b3defc295173caaf9c8833d6
Afflik <affliction134@gmail.com>
2019-04-09 01:25:54 +0300

    Up

---
+++ Lession4_Шепотько_Влад/Lession4/Task1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession4
+{
+    class Task1
+    {
+        static List<int> list = new List<int> ();
+        static int count = 20;
+        public static void Start()
+        {
+            list.Clear();
+            for (int i = 0; i < count; i++) list.Add(i);
+            Add();
+        }
+        public static void Add()
+        {
+            Console.WriteLine($"\nВ коллекции {list.Count} объектов. Пересоздать объекты в ней? Нажать Y/N");
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.Y:
+                    for (int i = count-1; i >= 0; i--) list.RemoveAt(i);
+                    count++;
+                    for (int i = 0; i < count; i++) list.Add(i);
+                    Add();
+                    break;
+                case ConsoleKey.N:
+                    Menu.Main();
+                    break;
+                default:
+                    Console.WriteLine("\nНеверна нажата клавиша!");
+                    Add();
+                    break;
+            }
+        }
+    }
+}
---
+++ Lession4_Шепотько_Влад/Lession4/Task3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession4
+{
+    class Task3
+    {
+        public static Dictionary<string, int> dict = new Dictionary<string, int>()
+  {
+    {"four",4 },
+    {"two",2 },
+    { "one",1 },
+    {"three",3 },
+  };
+        public static void Start()
+        {
+            var d = dict.OrderBy(x => x.Value);
+            foreach (var pair in d) Console.WriteLine($"{pair.Key} - {pair.Value}");
+            Console.ReadKey(true);
+            Start();
+        }
+    }
+}
